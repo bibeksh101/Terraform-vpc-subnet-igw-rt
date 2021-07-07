6ed9f6373ede33a8708076d76a650b939c6fded8
bibeksh101 <48139921+bibeksh101@users.noreply.github.com>
2021-07-07 01:29:14 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,6 @@ resource "aws_vpc" "myNewVPC" {
 resource "aws_subnet" "myvpcpublicsubnet1" {
   vpc_id = aws_vpc.myNewVPC.id
   cidr_block              = "10.0.101.0/24"
-  map_public_ip_on_launch = true
   availability_zone       = "us-east-2a"
   tags = {
     Name = "myvpcpublicsubnet1"
